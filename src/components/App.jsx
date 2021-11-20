aacdf4d481ace7a5543878920fbad10766e95b6d
aleksandr popov <sanchos8_7@mail.ru>
2021-11-20 07:51:04 +0300

    AddForm
    Добавление бургеров в меню

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,69 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import MenuAdmin from "./MenuAdmin";
+import sampleBurgers from "../sample-burgers";
+import Burger from "./Burger";
 
 class App extends React.Component {
+
+   state = {
+      burgers: {},
+      order: {}
+   };
+
+   addBurger = burger => {
+      console.log('addBurger', burger);
+      // 1. Создаем копию объекта state
+      const burgers = { ...this.state.burgers }
+      // 2. Добавляем новый бургер в объект burgers
+      burgers[`burger${Date.now()}`] = burger; // Date.now() - текущая дата в милисекундах
+      // 3. Записываем наш новый объект burgers в state
+      this.setState({ burgers });
+   };
+
+   loadSampleBurgers = () => {
+      this.setState({ burgers: sampleBurgers });
+   }
+
+   addToOrder = (key) => {
+      // 1. Делаем копию объекта state
+      const order = { ...this.state.order }
+      // 2. Добавить ключь к заказу со знач. 1, либо обновить текущее значение
+      order[key] = order[key] + 1 || 1;
+      // 3. Записываем наш новый объект order в state
+      this.setState({ order });
+   };
+
    render() {
       return (
          <div className="burger-paradise">
             <div className="menu">
+
                <Header title="Hot Burgers" />
+
+               <ul className="burgers">
+                  {/* Object.keys - получаем массив из ключей бургеров  */}
+                  {Object.keys(this.state.burgers).map(key => {
+                     return (
+                        <Burger
+                           key={key}
+                           index={key}
+                           details={this.state.burgers[key]}
+                           addToOrder={this.addToOrder}
+                        />
+                     )
+                  })}
+               </ul>
+
             </div>
-            <Order />
-            <MenuAdmin />
+            <Order
+               burgers={this.state.burgers}
+               order={this.state.order}
+            />
+            <MenuAdmin
+               addBurger={this.addBurger}
+               loadSampleBurgers={this.loadSampleBurgers}
+            />
          </div>
       );
    }
--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -2,9 +2,32 @@ import React from "react";
 
 class Order extends React.Component {
    render() {
+
+      // Получаем id заказов 
+      const orderIds = Object.keys(this.props.order);
+      // Считаем сколько всего бургеров заказали
+      const total = orderIds.reduce((prevTotal, key) => {
+         const burger = this.props.burgers[key];
+         const count = this.props.order[key];
+
+         // Считаем общую стоимость заказа и проверяем доступен ли он к заказу
+         const isAvailable = burger && burger.status === 'available';
+         if (isAvailable) {
+            return prevTotal + burger.price * count;
+         }
+         return prevTotal;
+      }, 0);
+
+
       return (
-         <div className="order-wrap">
+         <div className="order-wrap" >
             <h2>Ваш заказ</h2>
+            <ul className="order">
+               {orderIds.map(key => {
+                  return <li>{key}</li>
+               })}
+            </ul>
+            {total}
          </div>
       );
    }
